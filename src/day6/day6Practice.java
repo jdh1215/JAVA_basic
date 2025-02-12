940ae8b5cb4abb4daf6ff3086c8b08e9ab968cf8
jasper3745 <jasper528@naver.com>
2025-02-12 11:55:22 +0900

    day6

---
+++ src/day6/day6Practice.java
@@ -0,0 +1,42 @@
+package day6;
+
+import java.util.Scanner;
+
+public class day6Practice {
+    public static void main(String[] args) {
+        Scanner sc = new  Scanner(System.in);
+        //1번 문재
+//
+//        for (int i = 1; i <= 10; i++) {
+//            if ((i % 2) != 0) {
+//
+//                continue;
+//            }
+//            System.out.println("i = " + i);;
+//        }
+        //2번문재
+//        System.out.println("숫자를 입력하세요: ");
+//        int number = sc.nextInt();
+//        int result = 0;
+//        for (int i = 1; i <= number; i++) {
+//            result += i;
+//            if (result > 50) {
+//                System.out.println("합계가 50을 넘었습니다. 반복 종료!");
+//                break;
+//            }
+//            System.out.println("현재합계: " + result);
+//
+//
+//        }
+//        System.out.println("최종합계: " + result);
+        System.out.println("삼각형의 높이를 입력하세요: ");
+        int h = sc.nextInt();
+        for (int i = 1; i <= h; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print('*');
+
+            }
+            System.out.println();
+        }
+    }
+}
