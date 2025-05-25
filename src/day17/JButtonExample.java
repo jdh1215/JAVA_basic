ffd1c220eb61e46748b01a67dbaf7d0226ea0232
jdh1215 <kksuny100@gmail.com>
2025-05-25 11:29:11 +0900

    day15

--- src/day17/JButtonExample.java
+++ src/day17/JButtonExample.java
@@ -4,6 +4,13 @@ import javax.swing.*;
 public class JButtonExample {
     public static void main(String[] args) {
         JFrame frame = new JFrame("JButton 예제");
-        frame.setSize();
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        JButton button = new JButton("클릭하세요!");
+        panel.add(button);
+        frame.add(panel);
+        frame.setVisible(true);
     }
 }
